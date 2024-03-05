b6393266bb5dc690ddaf516f7af6d63abc5a79ec
yana <yanabgeorgieva@gmail.com>
2024-03-05 13:15:16 +0200

    Implemented Guest Details form functionality

--- solution.js
+++ solution.js
@@ -18,6 +18,51 @@ function changeContent(className) {
 
 document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
 
+/* the cleanData function needs to be reworked to prevent default behavior of the anchor element
 function cleanData(e) {
-    changeContent('search-form-content');
+    changeContent('guest-details-form-content');
 }
+*/
+// reworked cleanData function:
+function cleanData(e) {
+    e.preventDefault(); // Prevent default behavior of the anchor element
+    changeContent('guest-details-form-content');
+}
+
+//new code from admin/resources:
+document.querySelector('#guest-details-back-btn').addEventListener('click', (e) => fillRoomForm(e));
+
+function fillRoomForm(e) {
+    e.preventDefault();
+    changeContent('guest-details-form-content');
+}
+
+document.querySelector('#guest-details-next-btn').addEventListener('click', (e) => getPersonalData(e));
+
+function getPersonalData(e) {
+    e.preventDefault();
+    const data = e.target.parentElement.parentElement;
+
+    const name = data.querySelector('#name').value;
+    const phone = data.querySelector('#phone-number').value;
+    const email = data.querySelector('#email').value;
+
+    if (name != '' && phone != '' && email != '') {
+        reservation.name = name;
+        reservation.phone = phone;
+        reservation.email = email;
+        console.log(reservation);
+        changeContent('confirm-reservation-content');
+        fillConfirmReservationData(reservation);
+    }
+}
+
+function fillConfirmReservationData(customReservation) {
+    document.querySelector('.confirm-reservation #guest-name').textContent = `Name: ${customReservation.name}`;
+    document.querySelector('.confirm-reservation #guest-phone').textContent = `Phone Number: ${customReservation.phone}`;
+    document.querySelector('.confirm-reservation #guest-email').textContent = `Email: ${customReservation.email}`;
+    document.querySelector('.confirm-reservation #guest-room-type').textContent = `Room Type: ${customReservation.roomType}`;
+    document.querySelector('.confirm-reservation #guest-data-in').textContent = `Date-in: ${customReservation.startDate}`;
+    document.querySelector('.confirm-reservation #guest-data-out').textContent = `Date-out: ${customReservation.endDate}`;
+}
+
